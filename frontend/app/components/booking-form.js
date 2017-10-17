236be07a8fae6c1da8cf896d57d78fc083d17531
Akshay <akshaybabar2408@gmail.com>
2017-10-17 18:05:19 +0530

    Refactor code

--- frontend/app/components/booking-form.js
+++ frontend/app/components/booking-form.js
@@ -1,18 +1,25 @@
 import Ember from 'ember';
 
-export default Ember.Controller.extend({
-  start_at:Ember.computed.alias('model.start_at'),
-  end_at:Ember.computed.alias('model.end_at'),
+export default Ember.Component.extend({
+  buttonLabel: 'Save',
+  start_at:Ember.computed.alias('item.start_at'),
+  end_at:Ember.computed.alias('item.end_at'),
+  
   bookingDateChange: function() {
     if(this.get('start_at') && this.get('end_at')) {
       let date1 = new Date(this.get('start_at'));
       let date2 = new Date(this.get('end_at'));
       let timeDiff = Math.abs(date2.getTime() - date1.getTime());
       let diffDays = Math.ceil(timeDiff / (1000 * 3600 * 24)); 
-      this.set('model.price', (diffDays * this.get('rental.daily_rate')).toFixed(1)); 
+      this.set('item.price', (diffDays * this.get('rental.daily_rate')).toFixed(1)); 
       let rental_id = this.get('rental.id');
-      this.set('model.rental_id', rental_id);
+      this.set('item.rental_id', rental_id);
     }
   }.observes('start_at', 'end_at'),
 
+  actions: {
+    buttonClicked(param) {
+      this.sendAction('action', param);
+    }
+  }
 });
--- frontend/app/helpers/format-date.js
+++ frontend/app/helpers/format-date.js
@@ -5,16 +5,11 @@ export function formatDate(params, hash) {
 
   if (!date) {
     date = new Date();
-  } else {
-    date = date;
   }
 
   let year = date.getFullYear(),
     month = ('0' + (date.getMonth()+1)).slice(-2),
     day = ('0' + (date.getDate())).slice(-2),
-    /*hour = ('0' + (date.getHours())).slice(-2),
-    min = ('0' + (date.getMinutes())).slice(-2),
-    sec = ('0' + (date.getSeconds())).slice(-2),*/
     formatted = `${month}/${day}/${year}`;
 
   return formatted;
